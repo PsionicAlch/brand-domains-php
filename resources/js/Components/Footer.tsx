38faadc4713e4403e472c883ae849e79587fffa0
Jean-Jacques Strydom <jeanjacques.strydom34@gmail.com>
2024-08-03 23:47:44 +0200

    Made navbar responsive.

--- resources/js/Components/Footer.tsx
+++ resources/js/Components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="container mx-auto py-5 px-10 text-center text-neutral-500">
+        <footer className="container mx-auto py-5 px-10 text-center text-neutral-500 text-xs md:text-base">
             <p>Made with ❤️ by <a href="https://www.psionicalch.com" target="_blank" className="hover:text-neutral-800">PsionicAlch</a></p>
         </footer>
     )
