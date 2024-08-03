9a58454610ded19d2adbc0c173cc5617774a7560
Jean-Jacques Strydom <jeanjacques.strydom34@gmail.com>
2024-08-04 00:04:09 +0200

    Minor change

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Brand Domains';
 
 createInertiaApp({
     title: (title) => title ? `${title} - ${appName}` : `${appName} by PsionicAlch`,
