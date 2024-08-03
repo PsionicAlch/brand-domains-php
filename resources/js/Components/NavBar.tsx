5b5e05d33959df54fe0da0bd168d2153abb317a2
Jean-Jacques Strydom <jeanjacques.strydom34@gmail.com>
2024-08-03 23:03:45 +0200

    Made nav title a little smaller on sm screens

--- resources/js/Components/NavBar.tsx
+++ resources/js/Components/NavBar.tsx
@@ -3,14 +3,14 @@ export default function NavBar() {
         <header>
             <nav className="container mx-auto flex flex-col gap-3 px-5 md:gap-0 md:flex-row md:justify-between md:items-center mx:px-10 py-5">
                 <div>
-                    <h2 className="text-3xl font-bold">
+                    <h2 className="text-xl md:text-3xl font-bold">
                         <a
                             href={route('home')}
                         >
                             Brand Domains
                         </a>
                     </h2>
-                    <h3 className="text-xl font-light text-neutral-500">by{" "}
+                    <h3 className="text-sm md:text-xl font-light text-neutral-500">by{" "}
                         <a
                             href="https://www.psionicalch.com"
                             target="_blank"
